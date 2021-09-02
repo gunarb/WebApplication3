9a3158169d00073eeb308af52b5fe7ed9aca6b29
Gunnar Bolanos <gunnar.bolanos@wundermanthompson.com>
2021-09-01 18:12:48 -0600

    Add project files.

---
+++ Controllers/Components.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    public class Components : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListOfCars()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/IListofcarsItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace WebApplication3.Models
+{
+    public interface IListofcarsItem
+    {
+        string title { get; set; }
+        IList<IShowCars> carlist { get; set; }
+    }
+
+    public interface IShowCars
+    {
+        string brand { get; set; }
+        string model { get; set; }
+        string year { get; set; }
+    }
+}
